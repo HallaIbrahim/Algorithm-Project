97c5a8e35906d93f5db7ddb08bfda61d3cf9d280
Halla Ibrahim <98340013+HallaIbrahim@users.noreply.github.com>
2023-05-17 22:34:36 +0300

    Update Nestedloop.java

--- Nestedloop.java
+++ Nestedloop.java
@@ -7,12 +7,23 @@ int[]slots=new int[maxDeadline];
 
 // Sort jobs in decreasing order of profit in lambda expression by subtracting
 // adjacent elements a from b
-jobs.sort((a,b)->b.profit-a.profit);int numJobs=0,totalProfit=0;
+jobs.sort((a,b)->b.profit-a.profit);
+int numJobs=0,totalProfit=0;
 
 for(Job job:jobs){
 // Find the first available slot starting from the last job's deadline
-for(int i=job.deadline-1;i>=0;i--){if(slots[i]==0){slots[i]=1;numJobs++;totalProfit+=job.profit;break;}
+for(int i=job.deadline-1;i>=0;i--){
+  if(slots[i]==0){
+    slots[i]=1;
+    numJobs++;
+    totalProfit+=job.profit;
+    break;
+  }
 
-}}
+}
+}
 
-System.out.println("Number of jobs done: "+numJobs);System.out.println("Maximum profit: "+totalProfit);}}
+System.out.println("Number of jobs done: "+numJobs);
+System.out.println("Maximum profit: "+totalProfit);
+}
+}
